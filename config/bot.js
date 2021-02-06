ee98463cbf9c3b4f7766a8f428837511f3cdecc1
ScreaMdope <65433163+ScreaMdope@users.noreply.github.com>
2021-02-06 18:16:22 +0530

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: '📀',
+    },
+
+    discord: {
+        token: 'NzkzNTI5MzU2MjczMDU3ODAy.X-tlzQ.ZK5-t1wg9B6mwfOj03sXEEdzf9Y',
+        prefix: '*',
+        activity: 'PREFIX BY AAKUZZI',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
